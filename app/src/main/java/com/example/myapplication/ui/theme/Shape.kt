bfa8c2e97228412c566a9cb5c8f59c4957a629a4
DESKTOP-6LSHVT9\W10 <1223410028@qq.com>
2022-04-15 12:16:21 +0800

    自定义主题

--- app/src/main/java/com/example/myapplication/ui/theme/Shape.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Shape.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.ui.theme
 
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
@@ -8,4 +9,11 @@ val Shapes = Shapes(
         small = RoundedCornerShape(4.dp),
         medium = RoundedCornerShape(4.dp),
         large = RoundedCornerShape(0.dp)
+)
+
+
+val JetNewShapes = Shapes(
+        small = CutCornerShape(8.dp),
+        medium = CutCornerShape(topStart = 8.dp),
+        large = RoundedCornerShape(8.dp)
 )
--- app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
@@ -1,34 +1,59 @@
 package com.example.myapplication.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-        primary = Purple200,
-        primaryVariant = Purple700,
-        secondary = Teal200
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-        primary = Purple500,
-        primaryVariant = Purple700,
-        secondary = Teal200
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
 
-        /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
+    /* Other default colors to override
+background = Color.White,
+surface = Color.White,
+onPrimary = Color.White,
+onSecondary = Color.Black,
+onBackground = Color.Black,
+onSurface = Color.Black,
+*/
+)
+
+private val LightColors = lightColors( //浅色
+    primary = Red700,
+    primaryVariant = Red900,
     onPrimary = Color.White,
+    secondary = Red700,
+    secondaryVariant = Red900,
+    onSecondary = Color.White,
+    error = Red800
+)
+
+
+private val DarkColors = darkColors(
+    primary = Red300,
+    primaryVariant = Red700,
+    onPrimary = Color.Black,
+    secondary = Red300,
     onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    error = Red200
 )
 
 @Composable
-fun MyApplicationTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun MyApplicationTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
@@ -36,9 +61,27 @@ fun MyApplicationTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Com
     }
 
     MaterialTheme(
-            colors = colors,
-            typography = Typography,
-            shapes = Shapes,
-            content = content
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
+
+@Composable
+fun JetNewTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+
+    val colors = if (darkTheme) {
+        DarkColors
+    } else {
+        LightColors
+    }
+
+
+    MaterialTheme(
+        colors = colors,
+        typography = JetNewTypography,
+        shapes = JetNewShapes,
+        content = content
     )
 }
